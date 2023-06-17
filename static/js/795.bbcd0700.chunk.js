20ca12b5b0bdf9bba842dd3981db5d65ee142812
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-06-17 11:45:21 +0000

    Deploying to gh-pages from @ kuzkostia/goit-react-hw-08-phonebook@4e3901d38600521763ad5da2fbbabc27c791628c 🚀

---
+++ static/js/795.bbcd0700.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[795],{8795:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var o,r,c=t(168),h=t(7686),i=h.Z.div(o||(o=(0,c.Z)(["\n  padding: 38px 48px;\n  height: 100dvb;\n\n  background-color: lightblue;\n"]))),l=h.Z.h2(r||(r=(0,c.Z)(["\n    font-size: 28px;\n    text-align: center;\n    \n"]))),u=t(3329),a=function(){return(0,u.jsx)(i,{children:(0,u.jsx)(l,{children:"Welcome to Phone Book"})})}}}]);
+//# sourceMappingURL=795.bbcd0700.chunk.js.map
